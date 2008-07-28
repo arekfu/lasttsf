62feabe14976814f351bb332790c7341b33768b5
Davide Mancusi <d.mancusi@ulg.ac.be>
2008-07-28 21:22:57 +0200

    Initial commit of TSFscript
    A script to submit metadata about the tracks played by TSF Jazz
    (www.tsfjazz.com) to last.fm.

---
+++ tsfscript.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+
+############################################################################
+# Python-Qt template script for Amarok
+# (c) 2005 Mark Kretschmann <markey@web.de>
+#
+# Depends on: Python 2.2, PyQt
+############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+############################################################################
+
+import ConfigParser
+import os
+import sys
+import threading
+import signal
+import time
+
+try:
+    from qt import *
+except:
+    os.popen( "kdialog --sorry 'PyQt (Qt bindings for Python) is required for this script.'" )
+    raise
+
+
+# Replace with real name
+debug_prefix = "[TSF.fm]"
+
+class ConfigDialog( QDialog ):
+    """ Configuration widget """
+
+    def __init__( self ):
+        QDialog.__init__( self )
+        self.setWFlags( Qt.WDestructiveClose )
+        self.setCaption( "TSF.fm - Amarok" )
+
+        foo = None
+        try:
+            config = ConfigParser.ConfigParser()
+            config.read( "testrc" )
+            foo = config.get( "General", "foo" )
+        except:
+            pass
+
+        self.adjustSize()
+
+    def save( self ):
+        """ Saves configuration to file """
+
+        self.file = file( "testrc", 'w' )
+
+        self.config = ConfigParser.ConfigParser()
+        self.config.add_section( "General" )
+        self.config.set( "General", "foo", foovar )
+        self.config.write( self.file )
+        self.file.close()
+
+        self.accept()
+
+
+class Notification( QCustomEvent ):
+    __super_init = QCustomEvent.__init__
+    def __init__( self, str ):
+        self.__super_init(QCustomEvent.User + 1)
+        self.string = str
+
+class Test( QApplication ):
+    """ The main application, also sets up the Qt event loop """
+
+    def __init__( self, args ):
+        QApplication.__init__( self, args )
+        debug( "Started." )
+        self.track = None
+        self.oldtrack = None
+	self.radioMonitor = None
+
+        # Start separate thread for reading data from stdin
+        self.stdinReader = threading.Thread( target = self.readStdin )
+        self.stdinReader.start()
+
+        self.readSettings()
+
+    def readSettings( self ):
+        """ Reads settings from configuration file """
+
+        try:
+            foovar = config.get( "General", "foo" )
+
+        except:
+            debug( "No config file found, using defaults." )
+
+
+############################################################################
+# Stdin-Reader Thread
+############################################################################
+
+    def readStdin( self ):
+        """ Reads incoming notifications from stdin """
+
+        while True:
+            # Read data from stdin. Will block until data arrives.
+            line = sys.stdin.readline()
+
+            if line:
+                qApp.postEvent( self, Notification(line) )
+            else:
+                break
+
+
+############################################################################
+# Notification Handling
+############################################################################
+
+    def customEvent( self, notification ):
+        """ Handles notifications """
+
+        string = QString(notification.string)
+        debug( "Received notification: " + str( string ) )
+
+        if string.contains( "configure" ):
+            self.configure()
+
+        if string.contains( "engineStateChange: play" ):
+            self.engineStatePlay()
+
+        if string.contains( "engineStateChange: idle" ):
+            self.engineStateIdle()
+
+        if string.contains( "engineStateChange: pause" ):
+            self.engineStatePause()
+
+        if string.contains( "engineStateChange: empty" ):
+            self.engineStatePause()
+
+        if string.contains( "trackChange" ):
+            self.trackChange()
+
+# Notification callbacks. Implement these functions to react to specific notification
+# events from Amarok:
+
+    def configure( self ):
+        debug( "configuration" )
+
+        self.dia = ConfigDialog()
+        self.dia.show()
+        self.connect( self.dia, SIGNAL( "destroyed()" ), self.readSettings )
+
+    def engineStatePlay( self ):
+        """ Called when Engine state changes to Play """
+        pass
+
+    def engineStateIdle( self ):
+        """ Called when Engine state changes to Idle """
+        pass
+
+    def engineStatePause( self ):
+        """ Called when Engine state changes to Pause """
+        pass
+
+    def engineStateEmpty( self ):
+        """ Called when Engine state changes to Empty """
+        pass
+
+    def trackChange( self ):
+        """ Called when a new track starts """
+        stdin, stdout = os.popen2("dcop amarok player nowPlaying")
+	self.nowplaying = stdout.read().strip()
+	stdin.close()
+	stdout.close()
+        if self.nowplaying != "TSF Jazz":
+            try:
+	       self.radioMonitor.exit()
+	    except:
+	       pass
+        self.radioMonitor = threading.Thread( target = self.radio )
+        self.radioMonitor.start()
+
+    def radio( self ):
+        """ Connect to TSF Jazz and submit track information to Last.fm """
+	while True:
+            debug( "Downloading track information..." )
+            stdin, stdout = os.popen2("wget -O - --quiet http://www.tsfjazz.com/getSongInformations.php")
+	    self.track = stdout.read().strip()
+	    stdin.close()
+	    stdout.close()
+	    debug( "Done: " + self.track )
+            if self.track != self.oldtrack and self.oldtrack != None:
+                # Get track and artist name
+                pos = self.oldtrack.find("|")
+		if pos == -1:
+		    debug( "WARNING: Character '|' not found in track name!" )
+		    return
+		artist, title = self.oldtrack[:pos].title(), self.oldtrack[pos+1:].title()
+		debug( "Submitting track " + title + "by artist " + artist )
+		os.system( "/usr/lib/lastfmsubmitd/lastfmsubmit --artist '" + artist + "' --title '" + title + "'" )
+            self.oldtrack = self.track
+            time.sleep(30)
+
+
+############################################################################
+
+def debug( message ):
+    """ Prints debug message to stdout """
+
+    print debug_prefix + " " + message
+
+def main( ):
+    app = Test( sys.argv )
+
+    app.exec_loop()
+
+def onStop(signum, stackframe):
+    """ Called when script is stopped by user """
+    pass
+
+if __name__ == "__main__":
+    mainapp = threading.Thread(target=main)
+    mainapp.start()
+    signal.signal(15, onStop)
+    # necessary for signal catching
+    while 1: time.sleep(120)
